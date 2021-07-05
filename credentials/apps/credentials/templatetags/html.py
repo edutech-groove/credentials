e791a905d7b03f234012c9da2d0fdacad2b88839
Thin Nguyen <thin.nguyen@groovetechnology.com>
2021-07-05 13:54:08 +0700

    Update paths in templates

--- credentials/apps/credentials/templatetags/html.py
+++ credentials/apps/credentials/templatetags/html.py
@@ -1,7 +1,6 @@
 """
 Template tags and helper functions for escaping script tag.
 """
-import json
 from django import template
 from django.template.defaultfilters import date
 from django.utils.translation import get_language
@@ -30,16 +29,3 @@ def month(value):
         return formatted.lower()
 
     return formatted
-
-
-@register.filter
-def escape_json(data):
-    """
-    Escape a JSON string (or convert a dict to a JSON string, and then
-    escape it) for being embedded within an HTML template.
-    """
-    json_string = json.dumps(data) if isinstance(data, dict) else data
-    json_string = json_string.replace("&", "\\u0026")
-    json_string = json_string.replace(">", "\\u003e")
-    json_string = json_string.replace("<", "\\u003c")
-    return mark_safe(json_string)
